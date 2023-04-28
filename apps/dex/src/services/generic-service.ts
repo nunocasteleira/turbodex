83659481389c022aca0829a4de8bdc666d8dd762
Nuno Casteleira <nunojoaocas@gmail.com>
2023-04-28 22:00:01 +0100

    chore(dex): :rocket: try to fix environment variables v6

--- apps/dex/src/services/generic-service.ts
+++ apps/dex/src/services/generic-service.ts
@@ -12,8 +12,5 @@ export function getBaseUrl() {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV && process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
     return "https://" + process.env.NEXT_PUBLIC_VERCEL_URL;
   }
-  if (process.env.NEXT_PUBLIC_AWS_BRANCH && process.env.NEXT_PUBLIC_AWS_APP_ID) {
-    return "https://" + process.env.NEXT_PUBLIC_AWS_BRANCH + '.' + process.env.NEXT_PUBLIC_AWS_APP_ID + '.amplifyapp.com';
-  }
   return process.env.NEXT_PUBLIC_BASE_URL
 }
